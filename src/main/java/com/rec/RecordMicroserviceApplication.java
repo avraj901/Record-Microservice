7250272cb7ea2d9c718541835e31652cfa4a7907
unknown <cogjava3115@iiht.tech>
2022-08-19 10:38:30 +0530

    Added eurak client

--- src/main/java/com/rec/RecordMicroserviceApplication.java
+++ src/main/java/com/rec/RecordMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.rec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class RecordMicroserviceApplication {
 
 	public static void main(String[] args) {
